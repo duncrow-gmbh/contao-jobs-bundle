4297c003e2655abfa793efc4e4dc5bdc5e5cda19
Gerald Gradwohl <gerald@duncrow.com>
2024-05-15 10:31:36 +0200

    4.0.3 fixed hiringOrganisation. Option Name without " Option Object with "":"" like on schema.org

--- src/Resources/contao/languages/de/tl_job_settings.php
+++ src/Resources/contao/languages/de/tl_job_settings.php
@@ -1,5 +1,5 @@
 <?php
 
 $GLOBALS['TL_LANG']['tl_job_settings']['structuredData_legend'] = 'Strukturierte Daten';
-$GLOBALS['TL_LANG']['tl_job_settings']['contaojobsbundle_structuredData_hiringOrganization'] = array('hiringOrganization', 'siehe <a href="https://schema.org/hiringOrganization" target="_blank">https://schema.org/hiringOrganization</a>');
+$GLOBALS['TL_LANG']['tl_job_settings']['contaojobsbundle_structuredData_hiringOrganization'] = array('hiringOrganization', 'Name ohne Anführungszeichen, oder ein Object ("@type": "Organization", "name": "COMPANYNAME",...) : siehe <a href="https://schema.org/hiringOrganization" target="_blank">https://schema.org/hiringOrganization</a>');
 $GLOBALS['TL_LANG']['tl_job_settings']['contaojobsbundle_structuredData_jobLocation'] = array('jobLocation', 'siehe <a href="https://schema.org/jobLocation" target="_blank">https://schema.org/jobLocation</a>');
